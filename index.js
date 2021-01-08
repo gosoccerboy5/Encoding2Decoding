f9f4088c7d4246e2faef3a1e253cc29f7309fd21
Resince <49600150+Resince@users.noreply.github.com>
2021-01-07 18:48:40 -0800

    Update index.js

--- index.js
+++ index.js
@@ -2,20 +2,21 @@ const CHARACTERS ="abcdefghijklmnopqrstuvwxyz0123456789-_!@#$%^&*)(=+}{][<>?:,. 
 
 const encode = (input) => {
   for (i=0, res=''; i < input.length; i++) {
-    res += CHARACTERS.indexOf(input.charAt(i)) + 10;
+    const lc = input.toLowerCase()
+    res += CHARACTERS.indexOf(lc.charAt(i)) + 10;
   }
   return res;
-};
+}
 
 const decode = (input) => {
-  for (i=0, res=''; i < input.length; i+=2) {
-    const key = input.charAt(i).concat(input.charAt(i + 1));
-    res += CHARACTERS.charAt(key - 10);
+  for (i=0, res=''; i < input.length; i+= 2) {
+    const key = input.charAt(i).concat(input.charAt(i + 1))
+    res += CHARACTERS.charAt(key - 10)
   }
   return res;
-};
+}
 
 module.exports = {
   encode: encode,
   decode: decode
-};
+}
